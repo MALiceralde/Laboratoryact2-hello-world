525b94ce895006e0d83e3601481572871cc5bbd6
MALiceralde <146396314+MALiceralde@users.noreply.github.com>
2023-09-29 23:06:14 +0800

    Add files via upload

---
+++ miguelHelloWorld1.py
@@ -0,0 +1,12 @@
+yourname = input("What is your name? ")
+yourage = int(input("What is your age? "))
+age_next_year = yourage + 1
+
+mod = 5 % 3
+pi = 3.14159
+
+print(f"Hello, {yourname}" + "!")
+print(f"Your age by next year is: {age_next_year}")
+
+print("The remainder in 5 % 3 is", mod)
+print("The approximate value of pi is", pi)
